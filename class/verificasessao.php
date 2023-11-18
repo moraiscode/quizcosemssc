8c05ad39d5c7c4b578c5c00ca66f958a62af094b
Felipe Morais <contato.felipeamorim@gmail.com>
2023-11-18 13:39:35 -0300

    Clean Folder

--- class/verificasessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-// Verificar se o usuário não está logado
-if (!isset($_SESSION['usuario'])) {
-    header("Location: acessar.php");
-    exit();
-}
--- include/devby.php
+++
@@ -1,8 +0,0 @@
-<div class="py-6 px-6 text-center">
-    <div class="p-3"></div>
-    <p class=""><small>CFN®
-            <?php echo date("Y"); ?> | Desenvolvido por 💻 <a href="https://moraiscode.com/" target="_blank"
-                class="pe-1 text-primary font-weight-bold">Felipe Morais</a>
-        </small>
-    </p>
-</div>
